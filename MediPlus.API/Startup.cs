2eb0eee48462d7eff80835d1ada272ced8d19ea1
jarvis <jarvis.zhao@medi-plus.com.cn>
2019-12-16 09:54:31 +0800

    .net 版本

---
+++ MediPlus.API/Startup.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using MediPlus.Service;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using Newtonsoft.Json.Serialization;
+using MediPlus.Service.Base;
+using MediPlus.API.Filters;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using System.ComponentModel.Design;
+
+namespace MediPlus.API
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder().SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", false, true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+            .AddEnvironmentVariables()
+            .Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers(option =>
+            {
+                option.Filters.Add<ExceptionFilter>();
+                option.Filters.Add<ResultFilter>();
+            }).AddNewtonsoftJson(option =>
+            {
+                option.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                option.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                option.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+            });
+
+            //ContainerBuilder builder = new ContainerBuilder();
+            //builder.Populate(services);
+            services.ServiceInit();
+        }
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.ServiceInit();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}"
+                    );
+                endpoints.MapAreaControllerRoute(
+                     "areas", 
+                     "areas",
+                     "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllers();
+            });
+            app.UseStaticProvider();
+        }
+    }
+}
--- MediPlus.Web/Startup.cs
+++ MediPlus.Web/Startup.cs
@@ -1,90 +1,49 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using MediPlus.Service;
-using System.Text.Encodings.Web;
-using System.Text.Unicode;
-using Newtonsoft.Json.Serialization;
-using MediPlus.Service.Base;
-using MediPlus.API.Filters;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using System.ComponentModel.Design;
-
-namespace MediPlus.API
+namespace MediPlus.Web
 {
     public class Startup
     {
-        public Startup(IWebHostEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            Configuration = new ConfigurationBuilder().SetBasePath(env.ContentRootPath)
-            .AddJsonFile("appsettings.json", false, true)
-            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
-            .AddEnvironmentVariables()
-            .Build();
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            services.AddControllers(option =>
-            {
-                option.Filters.Add<ExceptionFilter>();
-                option.Filters.Add<ResultFilter>();
-            }).AddNewtonsoftJson(option =>
-            {
-                option.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                option.SerializerSettings.ContractResolver = new DefaultContractResolver();
-                option.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
-            });
-
-            //ContainerBuilder builder = new ContainerBuilder();
-            //builder.Populate(services);
-            services.ServiceInit();
-        }
-        public void ConfigureContainer(ContainerBuilder builder)
-        {
-            builder.ServiceInit();
-
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.Populate(services);
+             containerBuilder.
+            var container = containerBuilder.Build();
+            return new AutofacServiceProvider(container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    "default",
-                    "{controller=Home}/{action=Index}/{id?}"
-                    );
-                endpoints.MapAreaControllerRoute(
-                     "areas", 
-                     "areas",
-                     "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapControllers();
-            });
-            app.UseStaticProvider();
+            app.UseMvc();
         }
     }
 }
